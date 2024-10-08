3bf5ea0ab121295966f18619f56d8a4994f74fa6
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-10-07 23:35:35 -0700

    revise

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,98 @@
-import { AppBar } from "@mui/material";
-import React from "react";
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  Stack,
+  Toolbar,
+} from "@mui/material";
+import {
+  DarkMode,
+  LightMode,
+  Menu,
+  Person,
+  ShoppingCart,
+} from "@mui/icons-material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MyIconButton from "../Customized/MyIconButton";
+import { Link, NavLink } from "react-router-dom";
+import { appActions } from "../../store/slices/appSlice";
 
 export default function Navbar() {
+  const isMobile = useSelector((state) => state.app.isMobile);
+  const theme = useSelector((state) => state.app.theme);
+  const cart = useSelector((state) => state.cart);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const dispatch = useDispatch();
+
+  function handleThemeChange() {
+    if (theme == "dark") {
+      dispatch(appActions.setTheme("light"));
+    } else {
+      dispatch(appActions.setTheme("dark"));
+    }
+  }
+
   return (
     <AppBar
       position="sticky"
       sx={{ boxShadow: 2, bgcolor: "var(--pallete-background-paper)" }}
-    ></AppBar>
+    >
+      <Container fixed disableGutters>
+        <Toolbar>
+          {isMobile ? (
+            <Box flexGrow={1}>
+              <MyIconButton onClick={() => setOpenDrawer(true)}>
+                <Menu />
+              </MyIconButton>
+            </Box>
+          ) : (
+            <Stack
+              sx={{ py: 2, "& .active": { color: "info.light" } }}
+              flexGrow={1}
+              direction="row"
+              spacing={3}
+            >
+              <Button variant="text" LinkComponent={NavLink} to="/">
+                Home
+              </Button>
+              <Button variant="text" LinkComponent={NavLink} to="/shop">
+                Products
+              </Button>
+              <Button variant="text" LinkComponent={NavLink} to="/about">
+                About
+              </Button>
+            </Stack>
+          )}
+          <Stack direction="row" spacing={3}>
+            <MyIconButton onClick={() => setOpenMenu(!openMenu)}>
+              <Person />
+            </MyIconButton>
+            <MyIconButton onClick={handleThemeChange}>
+              {theme == "light" ? (
+                <DarkMode />
+              ) : (
+                <LightMode sx={{ color: "yellow" }} />
+              )}
+            </MyIconButton>
+            <Badge
+              badgeContent={cart.itemsCount()}
+              invisible={cart.itemsCount == 0}
+              color="secondary"
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+              overlap="circular"
+            >
+              <MyIconButton LinkComponent={Link} to="/cart">
+                <ShoppingCart />
+              </MyIconButton>
+            </Badge>
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,30 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoggedIn: false,
-    isAdmin: false,
-    profile: {},
+  isLoggedIn: false,
+  isAdmin: false,
+  profile: {},
 };
 
 const userSlice = createSlice({
-    name: "app",
-    initialState,
-    reducers: {
-        setIsLoggedIn(state, action) {
-            state.isLoggedIn = action.payload;
-        },
-        setIsAdmin(state, action) {
-            state.isAdmin = action.payload;
-        },
-        setProfile(state, action) {
-            state.profile = action.payload;
-        },
-        logout(state) {
-            state.isLoggedIn = false;
-            state.isAdmin = false;
-            state.profile = {};
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    setIsLoggedIn(state, action) {
+      state.isLoggedIn = action.payload;
     },
+    setIsAdmin(state, action) {
+      state.isAdmin = action.payload;
+    },
+    setProfile(state, action) {
+      state.profile = action.payload;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+      state.isAdmin = false;
+      state.profile = {};
+    },
+  },
 });
 
 export default userSlice;
